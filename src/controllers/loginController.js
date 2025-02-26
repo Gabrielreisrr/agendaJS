1ae4962b415ecd53c663a73c35451e763e1ec4d0
Gabriel de Souza Reis <gabrielreisctt1@gmail.com>
2025-02-26 19:59:35 -0300

    errors de cadastro corrigidos

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,36 +1,36 @@
 const Login = require("../models/LoginModel");
 
-exports.index = (req, res) => {
+exports.index = (req,res) => {
     res.render('login');
 };
 
-exports.register = async (req, res) => {
-    try{
+exports.register = async (req,res) => {
+    try {
         console.log("Entrou no register");
-    
+
         const login = new Login(req.body);
         await login.register();
-        
-        console.log("Login errors:", login.errors);
-    
+
+        console.log("Login errors:",login.errors);
+
         if (login.errors.length > 0) {
             console.log("Erros encontrados, salvando sessão...");
-            req.flash('errors', login.errors);
+            req.flash('errors',login.errors);
             req.session.save(() => {
                 console.log("Sessão salva, redirecionando para /login/index...");
-                
-                return res.redirect('/login/index'); 
+
+                return res.redirect('/login/index');
             });
             return;
         }
 
-        req.flash('success', 'Seu usuário foi criado com sucesso');
-        req.session.save(() => {    
-            return res.redirect('/login/index'); 
+        req.flash('success','Seu usuário foi criado com sucesso');
+        req.session.save(() => {
+            return res.redirect('/login/index');
         })
-    }catch(e){
+    } catch (e) {
         console.log(e);
         return res.render('404');
     };
-   
+
 };
