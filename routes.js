501ff05b140a25373b44809e62bb38e7e5aa353c
Gabriel de Souza Reis <gabrielreisctt1@gmail.com>
2025-03-06 20:57:15 -0300

    inicializando area de user

--- routes.js
+++ routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
-
+const contatoController = require('./src/controllers/contatoController');
+const { loginRequired } = require('./src/middlewares')
 
 // Rotas da home
 route.get('/',homeController.index);
@@ -14,7 +15,7 @@ route.post('/login/login',loginController.login);
 route.get('/login/logout',loginController.logout);
 
 //Rotas de contato
-
+route.get('/contato/index',contatoController.index);
 
 
 module.exports = route;
